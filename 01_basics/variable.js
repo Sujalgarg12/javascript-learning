beadc66aab04b4c47026a44c28568c0e01a9e8d3
Sujalgarg12 <119889002+Sujalgarg12@users.noreply.github.com>
2024-01-09 12:06:34 +0000

    var and datatype in js

---
+++ 01_basics/variable.js
@@ -0,0 +1,15 @@
+const accountId= 1233353
+let accountName="Sujal garg"
+var accountCity=("Jaipur")
+
+// NOTE-: We use console.log for a single variable printing. 
+
+console.log(accountId);
+console.log(accountCity)
+
+// accountId= 4334544
+
+// NOTE -: Here we use console.log for  a multiple variable printing at a time in a table format.
+
+
+console.table([accountId, accountCity, accountName])
