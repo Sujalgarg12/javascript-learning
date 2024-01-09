03e9976ca7e19e6da24fab172dc5fd296c13e82b
Sujalgarg12 <119889002+Sujalgarg12@users.noreply.github.com>
2024-01-09 12:38:49 +0000

    Datatypes in js

---
+++ 01_basics/Datatypes.js
@@ -0,0 +1,36 @@
+// alert("hello sujal garg") we are using node.js
+
+console.log("sujal garg"), console.log(1424465762)   
+// It gives wrong impression.   It removes the code readeability.
+
+// Original Specification of any language or words are from --ECMA or tc39.es
+
+// --------------------------------------------DATATYPES---------------------------------------------
+// string datatype
+
+let Name= "sujal garg"  
+ console.log(Name)
+// int datatype
+
+let num= 15453
+console.log(num)
+// Boolean Datatype
+let isLoggedIn= false
+
+// PRIMITUVE DATATYPES
+{
+// number => 2 to power 53
+// bigint
+// string=> ""
+// boolean => true/false
+// null => standalone value/empty value.
+// undefined => used when variable is defined but that's value not defined.
+// symbol => find uniqueness.
+}
+
+// Object Datatypes
+
+console.log(typeof(6646))
+console.log(typeof "sujal garg")
+console.log(typeof null)
+console.log(typeof undefined)
