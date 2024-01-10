17e8daade25856c714f47ad27421febc9f3c4c0c
Sujalgarg12 <119889002+Sujalgarg12@users.noreply.github.com>
2024-01-10 19:57:50 +0000

    Datastype Conversion-Operation in js

---
+++ 01_basics/Conversion-Operation.js
@@ -0,0 +1,28 @@
+let score= "332"
+let Score= "342dfdgd"
+console.log(score)
+console.log(Score)
+console.log(typeof score)
+console.log(typeof(Score))
+  
+                        //   NUMBER  conversion
+ 
+let value= Number(score)
+console.log(value)
+console.log(typeof value)
+console.log(typeof(value))
+                        // STRING Conversion
+
+let Value= String(score)
+console.log(Value)
+console.log(typeof Value)
+console.log(typeof(Value))
+
+let StrConverse= Number(Score)
+console.log(StrConverse)
+console.log(typeof StrConverse)
+
+let Null= null
+console.log(Null)
+let NullConverse= Number(Null)
+console.log(NullConverse)
